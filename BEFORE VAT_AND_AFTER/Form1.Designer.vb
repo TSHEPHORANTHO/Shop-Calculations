00b2311f78df55bd8a41a2f946423a94774d08f0
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-06 22:51:30 +0200

    Add project files.

---
+++ BEFORE VAT_AND_AFTER/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnCalculate = New System.Windows.Forms.Button()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnCalculate
+        '
+        Me.BtnCalculate.Location = New System.Drawing.Point(294, 355)
+        Me.BtnCalculate.Name = "BtnCalculate"
+        Me.BtnCalculate.Size = New System.Drawing.Size(234, 58)
+        Me.BtnCalculate.TabIndex = 0
+        Me.BtnCalculate.Text = "Calculate"
+        Me.BtnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(165, 114)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 26)
+        Me.txtPrice.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Price"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(165, 159)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(100, 26)
+        Me.txtQuantity.TabIndex = 1
+        Me.txtQuantity.Text = " "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Quantity"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.BtnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnCalculate As Button
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ BEFORE VAT_AND_AFTER/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles BtnCalculate.Click
+        Dim Price As Decimal
+        Dim Quantity As Decimal
+        Dim Total As Decimal
+        Dim Calculate As String
+
+        'Get Prices 
+        Price = txtPrice.Text
+        Quantity = txtQuantity.Text
+        Calculate = BtnCalculate.Text
+
+        'Total cost
+        Total = Price * Quantity
+
+
+        'Message
+        MsgBox("Your Total is " & " R" & Total)
+
+
+    End Sub
+End Class
